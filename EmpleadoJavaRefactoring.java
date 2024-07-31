65abc06537e224b5624e2ebde55b309d7906cfe1
hailie <142462676+se0klie@users.noreply.github.com>
2024-07-31 15:54:06 -0500

    Create EmpleadoJavaRefactoring.java

---
+++ EmpleadoJavaRefactoring.java
@@ -0,0 +1,86 @@
+public class Empleado {
+    private String nombre;
+    private double salarioBase;
+    private int horasTrabajadas;
+    private Departamento departamento;
+    private double tarifaHora;
+
+    public Empleado(){}
+    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, Departamento departamento) {
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        this.horasTrabajadas = horasTrabajadas;
+        this.tarifaHora = tarifaHora;
+        this.departamento = departamento;
+    }
+
+    public double calcularSalario() { // replace nested conditions with guard clauses
+        double salarioTotal=0;
+
+        if (isValidSalary()){
+           salarioTotal=calculateFromHours();
+        } else {
+            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
+        }
+        salarioTotal = departamento.getSalary(salarioTotal);
+        
+        return salarioTotal;
+    }
+
+    public double calculateFromHours(){
+        double result = salarioBase;
+        if(horasTrabajadas>40){
+            result += (horasTrabajadas - 40) *50.0;
+        } 
+        return result;
+    }
+
+    
+    
+    public boolean isValidSalary(){
+        return salarioBase>0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public double getTarifaHora() {
+        return tarifaHora;
+    }
+
+    public void setTarifaHora(double tarifaHora) {
+        this.tarifaHora = tarifaHora;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+
+    // Más metodos
+}
